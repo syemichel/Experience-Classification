23e358f713b37cf782803f2df3652d0a7fcedd6c
syemichel <60457772+syemichel@users.noreply.github.com>
2024-04-30 17:38:42 +0800

    Update train.py

--- source code/train.py
+++ source code/train.py
@@ -1,44 +1,20 @@
-import csv
-
 from PRG_SB3 import *
-from stable_baselines3.common.evaluation import evaluate_policy
-import argparse
 from pyRDDLGym import RDDLEnv
-from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
-from Experience_classify import SAC_EC
+from EC import SAC_EC
 from stable_baselines3.common.logger import configure
-from stable_baselines3.common.callbacks import EvalCallback
-import time
 import sys
 sys.setrecursionlimit(1000000)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', type=int, default=1000000, help='train num')
-    parser.add_argument('-r', type=str, default='task9', help='rddl name')
-    parser.add_argument('-i', type=str, default='inst1', help='inst name')
-    parser.add_argument('-l', type=str, default='waterworld1', help='inst name')
-    parser.add_argument('-log', type=str, default='task9', help='inst name')
-    parser.add_argument('-a', type=float, default=0.75, help='sample alpha')
-    parser.add_argument('-k', type=int, default=1, help='sample alpha')
-    args = parser.parse_args()
-
-    rddl_name = args.r
-    inst_name = args.i
-    train_num = args.n
-    log = args.log
-    l = args.l
-    a = args.a
-    log_inter = 20
-    k = args.k
-    domain = 'benchmarks/' + rddl_name + '/' + l + '.rddl'
-    instance = 'benchmarks/' + rddl_name + '/' + inst_name + '.rddl'
-    log_path = log + '/' + inst_name + '_' + str(k) + '/conv.csv'
+    domain = "benchmarks/waterworld3/waterworld3.rddl" # path for domain
+    instance = "benchmarks/waterworld3/inst21.rddl" # path for instance
+    log_path = "progress.csv" # path for log
 
     env = RDDLEnv.RDDLEnv(domain=domain, instance=instance)
     env = FlattenAction(env)
     model = SAC_EC("MultiInputPolicy", env, verbose=1, learning_starts=1000,
-                        learning_rate=3e-4, batch_size=256, train_freq=1, device='cpu')
-    new_logger = configure(log + '/' + inst_name + '_' + str(k), ["csv"])
+                        learning_rate=3e-4, batch_size=256, train_freq=1, device='cpu')  
+    # SAC_EC can be changed by other algorithms in EC/RS/PER/MOD 
+    new_logger = configure(log_path, ["csv"])
     model.set_logger(new_logger)
-    model.learn(total_timesteps=train_num, log_interval=log_inter)
+    model.learn(total_timesteps=500000, log_interval=100) # set training steps and log interval
